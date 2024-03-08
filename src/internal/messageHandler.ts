2a9dd1609c104cf763bf634610748d1d379f1959
Max Sporer <maxsporer@Maxs-MacBook-Pro.local>
2024-03-07 23:00:15 -0500

    add functionality for bot to repeat chat messages

--- src/internal/messageHandler.ts
+++ src/internal/messageHandler.ts
@@ -4,10 +4,21 @@ import { ResponseWrapper } from "./responseWrapper";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
-  req: HttpRequest
+  req: HttpRequest,
 ): Promise<any> {
   const res = new ResponseWrapper(context.res);
   await notificationApp.requestHandler(req, res);
+  const pageSize = 100;
+  let continuationToken: string | undefined;
+  do {
+    const pagedInstallations = await notificationApp.notification.getPagedInstallations(pageSize, continuationToken);
+    continuationToken = pagedInstallations.continuationToken;
+    const targets = pagedInstallations.data;
+    for (const target of targets) {
+      target.sendMessage(context.bindings.req.body.text);
+    }
+  } while (continuationToken);
+
   return res.body;
 };
 
